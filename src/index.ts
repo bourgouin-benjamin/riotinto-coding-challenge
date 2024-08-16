1888d9cc415c9ebfe325405265776c667b610130
Benjamin Bourgouin <benjamin.bourgouin.pro@gmail.com>
2024-08-16 02:45:19 -0400

    Seprating app and server from index

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
-import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { itemsRouter } from "./items/items.controller";
 
-dotenv.config();
-
-if (!process.env.PORT) {
-    process.exit(1);
-}
-const PORT: number = parseInt(process.env.PORT as string, 10);
- 
 const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/items", itemsRouter);
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+export default app;
---
+++ src/server/server.ts
@@ -0,0 +1,13 @@
+import app from "../index";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+    process.exit(1);
+}
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+app.listen(PORT, () => {
+    console.log(`Server in running on port ${PORT}`);
+});
